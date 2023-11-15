10acd40df3ee35559eeb86029023b317f1286bcc
Evader-lgb <1412343257@qq.com>
2023-11-16 07:33:29 +0800

    feat:redis yml config

--- jayden-user/src/main/java/com/jayden/user/controller/TestController.java
+++ jayden-user/src/main/java/com/jayden/user/controller/TestController.java
@@ -2,6 +2,8 @@ package com.jayden.user.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "测试")
 public class TestController {
 
+    @Autowired
+    RedisTemplate redisTemplate;
+
     @GetMapping("/test")
     @ApiOperation(value = "测试")
     public String test() {
         return "test";
     }
+
+    @GetMapping("/testRedis")
+    public String testRedis() {
+        redisTemplate.opsForValue().set("name", "qj");
+        String name = (String) redisTemplate.opsForValue().get("name");
+
+        return name;
+    }
+
+
 }
