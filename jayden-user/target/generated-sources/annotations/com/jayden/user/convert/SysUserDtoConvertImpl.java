5023b44ea6b7f9b1f1f1f3516b389fa9ee10d4b1
Evader-lgb <1412343257@qq.com>
2023-11-18 13:29:43 +0800

    feat:集成log4j

--- jayden-user/target/generated-sources/annotations/com/jayden/user/convert/SysUserDtoConvertImpl.java
+++ jayden-user/target/generated-sources/annotations/com/jayden/user/convert/SysUserDtoConvertImpl.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-11-16T07:29:21+0800",
+    date = "2023-11-18T13:16:05+0800",
     comments = "version: 1.4.0.Final, compiler: javac, environment: Java 11.0.19 (Eclipse Adoptium)"
 )
 public class SysUserDtoConvertImpl implements SysUserDtoConvert {
